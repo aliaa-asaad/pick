36cf354f4bb9ad1f3ddf92faabe4836c04e1764f
aliaa-asaad <aliaaasaad35@gmail.com>
2023-11-16 15:04:32 +0200

    profile ui editing

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -110,111 +110,116 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: SafeArea(
-        child: Column(children: [
-          SizedBox(
-            height: MediaQueryHelper.height * .059,
-          ),
-          CircleAvatar(
-            radius: 55.r,
-            backgroundImage: NetworkImage(SharedHandler.instance!.getData(
-                        key: SharedKeys().userType, valueType: ValueType.int) ==
-                    0
-                ? SharedHandler.instance!.getData(
-                    key: SharedKeys().user,
-                    valueType: ValueType.map)['imageUrl']
-                : SharedHandler.instance!.getData(
-                    key: SharedKeys().driver,
-                    valueType: ValueType.map)['imageUrl']),
-          ),
-          SizedBox(
-            height: MediaQueryHelper.height * .01,
-          ),
-          Text(
-            SharedHandler.instance!.getData(
-                        key: SharedKeys().userType, valueType: ValueType.int) ==
-                    0
-                ? SharedHandler.instance!.getData(
-                    key: SharedKeys().user,
-                    valueType: ValueType.map)['fullName']
-                : SharedHandler.instance!.getData(
-                    key: SharedKeys().driver,
-                    valueType: ValueType.map)['fullName'],
-            style: TextStyleHelper.subtitle17.copyWith(color: Colors.white),
-          ),
-          SizedBox(
-            height: MediaQueryHelper.height * .01,
-          ),
-          Text(
-            SharedHandler.instance!.getData(
-                        key: SharedKeys().userType, valueType: ValueType.int) ==
-                    0
-                ? SharedHandler.instance!.getData(
-                    key: SharedKeys().user,
-                    valueType: ValueType.map)['phoneNumber']
-                : SharedHandler.instance!.getData(
-                    key: SharedKeys().driver,
-                    valueType: ValueType.map)['phoneNumber'],
-            style: TextStyleHelper.subtitle17.copyWith(color: Colors.white),
-          ),
-          const Spacer(),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 16.h),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(16.r),
-              ),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: MediaQueryHelper.height * .059,
             ),
-            width: MediaQueryHelper.width,
-            height: MediaQueryHelper.height * .53,
-            child: Column(
-              children: List.generate(
-                content.length,
-                (index) => InkWell(
-                  onTap: () {
-                    if (index == content.length - 1) {
-                      AuthBloc.instance.add(LogoutClick());
-                    } else if (index == content.length - 2) {
-                      _showMyDialog(context);
-                    } else {
-                      AppRoutes.pushNamedNavigator(
-                          routeName: content[index]['route']);
-                    }
-                  },
-                  child: ListTile(
-                    minLeadingWidth: 20.w,
-                    minVerticalPadding: MediaQueryHelper.height * .008,
-                    trailing: Icon(
-                      Platform.isIOS
-                          ? Icons.arrow_forward_ios
-                          : Icons.arrow_forward,
-                      size: MediaQueryHelper.height * .025,
-                      color: index == content.length - 1
-                          ? Colors.red
-                          : Theme.of(context).colorScheme.primary,
-                    ),
-                    title: Text(
-                      "${content[index]['title']}",
-                      style: TextStyleHelper.subtitle17.copyWith(
-                        fontWeight: FontWeight.bold,
+            CircleAvatar(
+              radius: 55.r,
+              backgroundImage: NetworkImage(SharedHandler.instance!.getData(
+                          key: SharedKeys().userType, valueType: ValueType.int) ==
+                      0
+                  ? SharedHandler.instance!.getData(
+                      key: SharedKeys().user,
+                      valueType: ValueType.map)['imageUrl']
+                  : SharedHandler.instance!.getData(
+                      key: SharedKeys().driver,
+                      valueType: ValueType.map)['imageUrl']),
+            ),
+            SizedBox(
+              height: MediaQueryHelper.height * .01,
+            ),
+            Text(
+              SharedHandler.instance!.getData(
+                          key: SharedKeys().userType, valueType: ValueType.int) ==
+                      0
+                  ? SharedHandler.instance!.getData(
+                      key: SharedKeys().user,
+                      valueType: ValueType.map)['fullName']
+                  : SharedHandler.instance!.getData(
+                      key: SharedKeys().driver,
+                      valueType: ValueType.map)['fullName'],
+              style: TextStyleHelper.subtitle17.copyWith(color: Colors.white),
+            ),
+            SizedBox(
+              height: MediaQueryHelper.height * .01,
+            ),
+            Text(
+              SharedHandler.instance!.getData(
+                          key: SharedKeys().userType, valueType: ValueType.int) ==
+                      0
+                  ? SharedHandler.instance!.getData(
+                      key: SharedKeys().user,
+                      valueType: ValueType.map)['phoneNumber']
+                  : SharedHandler.instance!.getData(
+                      key: SharedKeys().driver,
+                      valueType: ValueType.map)['phoneNumber'],
+              style: TextStyleHelper.subtitle17.copyWith(color: Colors.white),
+            ),
+          //  const Spacer(),
+          SizedBox(
+              height: MediaQueryHelper.height * .04,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16.h),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(16.r),
+                ),
+              ),
+              width: MediaQueryHelper.width,
+              height: MediaQueryHelper.height * .53,
+              child: Column(
+                children: List.generate(
+                  content.length,
+                  (index) => InkWell(
+                    onTap: () {
+                      if (index == content.length - 1) {
+                        AuthBloc.instance.add(LogoutClick());
+                      } else if (index == content.length - 2) {
+                        _showMyDialog(context);
+                      } else {
+                        AppRoutes.pushNamedNavigator(
+                            routeName: content[index]['route']);
+                      }
+                    },
+                    child: ListTile(
+                      minLeadingWidth: 20.w,
+                      minVerticalPadding: MediaQueryHelper.height * .008,
+                      trailing: Icon(
+                        Platform.isIOS
+                            ? Icons.arrow_forward_ios
+                            : Icons.arrow_forward,
+                        size: MediaQueryHelper.height * .025,
                         color: index == content.length - 1
                             ? Colors.red
-                            : const Color(0xff4F5E7B),
+                            : Theme.of(context).colorScheme.primary,
                       ),
-                    ),
-                    leading: SvgPicture.asset(
-                      content[index]['icon'],
-                      height: MediaQueryHelper.height * .025,
-                      color: index == content.length - 1
-                          ? Colors.red
-                          : Theme.of(context).colorScheme.primary,
+                      title: Text(
+                        "${content[index]['title']}",
+                        style: TextStyleHelper.subtitle17.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: index == content.length - 1
+                              ? Colors.red
+                              : const Color(0xff4F5E7B),
+                        ),
+                      ),
+                      leading: SvgPicture.asset(
+                        content[index]['icon'],
+                        height: MediaQueryHelper.height * .025,
+                        color: index == content.length - 1
+                            ? Colors.red
+                            : Theme.of(context).colorScheme.primary,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
